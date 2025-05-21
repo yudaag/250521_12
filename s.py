1268ae3a72f54a74a9d4010e2335c557e30544ed
yudaag <151615035+yudaag@users.noreply.github.com>
2025-05-21 10:15:18 +0900

    s.py

--- s.py
+++ s.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.vectorstores import FAISS
